2df2ad45e1622f5a34e5ebeb7a59007a29661191
karisluo <karisluo@ifastfinancial.com.cn>
2018-10-11 15:58:54 +0800

    添加按钮

--- App.js
+++ App.js
@@ -6,24 +6,32 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+    onPress = () => {
+      // codePush.sync({
+      //   updateDialog: true,
+      //   installMode: codePush.InstallMode.IMMEDIATE
+      // });
+    };
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Karis yayayay~ mua~~ mememem~~</Text>
+        <Text style={styles.instructions}>更新哦！！</Text>
+        <TouchableOpacity onPress={this.onPress}>
+          <Text>检查要不要更新哦</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -32,18 +40,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
